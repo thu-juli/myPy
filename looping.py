5b68e6752f8fddcda912c1741da24318e25bb5d2
thujuli <thujuli037@gmail.com>
2021-12-11 00:33:30 +0800

    Looping for

---
+++ looping.py
@@ -0,0 +1,12 @@
+"""
+Program perulangan membaca buku
+"""
+jumlah_buku = 10
+print('Ibu berkata: "Baca semua bukumu')
+
+jumlah_buku_yang_sudah_dibaca = 0
+
+print(f'Jumlah buku yang sudah saya baca {jumlah_buku_yang_sudah_dibaca}')
+for jumlah_buku_yang_sudah_dibaca in range (1, jumlah_buku+1):
+    print (f'Jumlah buku yang sudah dibaca {jumlah_buku_yang_sudah_dibaca}')
+print(f'Jumlah buku yang sudah saya baca {jumlah_buku_yang_sudah_dibaca}')
