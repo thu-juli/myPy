986300eedda6da92abed75f9f6d41b084bdafacf
thujuli <thujuli037@gmail.com>
2021-12-10 15:21:05 +0800

    My first Python code!

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 """
-Ini adalah kode Python pertama saya!!
+Semua sintaksis dasar bahasa pemrograman terdiri dari:
+1. Sekuensial: Langkah berurutan
+2. Percabangan: Langkah melompat jika kondisi terpenuhi
+3. Perulangan: Mengulang langkah yang sama berkali-kali selama/sampai kondisi terpenuhi
 """
-print("Hello World!")
-print("My Name Is Thu Juli")
+
+# Sekuensial
+print('Ibu berkata: "Budi pergi ke toko!"')
+print('Budi menjawab: "Apa yang harus saya lakukan di toko"')
+print('Ibu menjawab: "Beli 1 botol susu jika ada telor, beli 6"')
+print('Maka budi berangkat ke toko')
+print('Dan budi mulai berbelanja')
