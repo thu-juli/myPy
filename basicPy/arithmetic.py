38cb8d9254e45fef035579b866a49c500aad3375
thujuli <thujuli037@gmail.com>
2021-12-12 22:23:14 +0800

    operation arithmetic

---
+++ basicPy/arithmetic.py
@@ -0,0 +1,56 @@
+# aritmetic operations
+
+a = 10
+b = 5
+
+# addition +
+results = a + b
+print('====Addition====')
+print(a, '+', b, '=', results)
+
+# subtraction -
+results = a - b
+print('====Subtraction====')
+print(a, '-', b, '=', results)
+
+# multiplication *
+results = a * b
+print('====Multiplication====')
+print(a, '*', b, '=', results)
+
+# divison /
+results = a / b
+print('====Divison====')
+print(a, '/', b, '=', results)  # type data auto generate to float
+
+# exponentiation **
+results = a ** b
+print('====Exponentiation====')
+print(a, '**', b, '=', results)
+
+# modulus %
+results = a % b
+print('====Modulus====')
+print(a, '%', b, '=', results)
+
+# floor division //
+results = a // b
+print('====Floor Divison====')
+print(a, '//', b, '=', results)  # floor function after division
+
+# operation priority
+print('====Operation Priority====')
+# priority
+##
+x = 3
+y = 2
+z = 4
+
+results = x ** y * z + x / y - y % z // x
+print(x, '**', y, '*', z, '+', x, '/', y, '-', y, '%', z, '//', x, '=', results)
+
+results = x + y * z
+print(x, '+', y, '*', z, 'results =', results)
+
+results = (x + y) * z
+print('(', x, '+', y, ')', '*', z, 'results =', results)
