cd7af1464564751a989262a96f458e600ee75a00
thujuli <thujuli037@gmail.com>
2021-12-13 19:05:13 +0800

    Temperature Conversion

---
+++ basicPy/simpleCalculation.py
@@ -0,0 +1,21 @@
+# temperature conversion exercise
+
+# program convert celcius
+
+print('\nProgram Temperature Conversion\n')
+
+celcius = float(input('input temperature celcius: '))
+print('temperature', celcius, 'degress celcius')
+
+
+# reamur 4/5 * c
+reamur = (4/5) * celcius
+print('temperature', reamur, 'degress reamur')
+
+# fahrenheit 9/5 * c + 32
+fahrenheit = ((9/5) * celcius) + 32
+print('temperature', fahrenheit, 'degress fahrenheit')
+
+# kelvin celcius + 273
+kelvin = celcius + 273
+print('temperaute', kelvin, 'degress kelvin')
