acd56ab8f75f6807d927a45375c545ceee4578bb
thujuli <thujuli037@gmail.com>
2021-12-13 20:26:05 +0800

    comparison operation

---
+++ basicPy/comparisonOperation.py
@@ -0,0 +1,25 @@
+# Comparison Operation
+
+# all results are boolean
+# >,<,>=,<=,==,!=,is,is not
+
+a = 4
+b = 2
+
+# greater than >
+print('===Greater Than====')
+result = a > 3
+print(a, ">", 3, '=', result)
+result = b > 3
+print(b, ">", 3, '=', result)
+result = b > 2
+print(b, ">", 2, '=', result)
+
+# less than >
+print('\n===Less Than====')
+result = a < 3
+print(a, "<", 3, '=', result)
+result = b < 3
+print(b, "<", 3, '=', result)
+result = b < 2
+print(b, "<", 2, '=', result)
