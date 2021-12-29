4443811e54c47f3f8f57bb2b73c0e2259c582b58
thujuli <thujuli037@gmail.com>
2021-12-29 10:49:57 +0800

    day2 start remember yesterday

---
+++ today/day2.py
@@ -0,0 +1,31 @@
+# Yesterday
+
+#Tipe Data
+
+## INTEGER
+a = 10  # Tipe data integer (Number) This is Variable
+
+print("===INTEGER===") # DECORATE
+print("Data a:", a)  # This Simple Command Print
+print("Type Data a:", type(a)) # Simple comand Print Type data
+
+## FLOAT
+a = 10.0  # Tipe data Float (Decimal) This is Variable
+
+print("===FLOAT===") # DECORATE
+print("Data a:", a)  # This Simple Command Print
+print("Type Data a:", type(a)) # Simple comand Print Type data
+
+## STRING
+a = "10"  # Tipe data string (text/char)This is Variable
+
+print("===STRING===") # DECORATE
+print("Data a:", a)  # This Simple Command Print
+print("Type Data a:", type(a)) # Simple comand Print Type data
+
+## BOOLEAN
+a = True  # Tipe data bolean (True/False) This is Variable
+
+print("===BOOLEAN===") # DECORATE
+print("Data a:", a)  # This Simple Command Print
+print("Type Data a:", type(a)) # Simple comand Print Type data
