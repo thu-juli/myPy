9932c1a8be0ddf12f2055a271fbe986e1c68035f
thujuli <thujuli037@gmail.com>
2022-01-01 17:07:08 +0800

    remember variable

--- today/day3.py
+++ today/day3.py
@@ -1,1 +1,8 @@
-print("Hello wolrd!")
+# Variable
+
+a = 10 # Simple assigment variable
+print(a)
+print("======")
+
+b = a  #  Simple assigme varibale indirect
+print("value data b:", b) # test print value data b
