df18650f79ef3628e9a2ee8eab877338bc0d498a
thujuli <thujuli037@gmail.com>
2022-01-03 07:10:20 +0800

    remember comparasion operaion

--- today/day3.py
+++ today/day3.py
@@ -1,37 +1,16 @@
-# aritmeth operation
-# in python availible artitmeth operation ((),**,*,/,%,//,+,-)
+# Comprasion Operation
 
-"""
-() > Parenthesis use piority number 1
-** > Use Exponent 
-* > Use Multiple
-/ > Use Division
-% > Use Modulus
-// > Use Flor Division
-+ > Use increase
-- > Use decrease
-"""
+# a = 2 
+# b = 4
 
-# Exercise day3
-# Convert day to time
-"""
-Input
-1. Command to user input day 
+# result = a <= 2
+# print(a,">",2,"=", result)
 
-Process
-1. Process day to hours, minute and sec
+a = 10
 
-Result
-1. Day in hours, minute and sec
-"""
-print("===Convert day to time===")
-input_user = float(input("input day your choose(in number): ")) # Input and convert in same command
+result = a == 10  # Comparasion operation for literal
+print(a,"==",10,"=",result)
 
-in_hour = input_user * 24
-in_minute = in_hour * 60
-in_sec = in_minute * 60
-
-print("==RESULT==")
-print("day in hours:", in_hour, "h")
-print("day in minutes:", in_minute, "m")
-print("day in seconds:", in_sec, "sec")
+b = 10
+result = a is b  # Comparasion operation for variable
+print("a is b =", result)
