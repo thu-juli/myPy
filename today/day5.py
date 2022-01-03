a18ae6d92ffe7699932a7fb1816de501151f9f9a
thujuli <thujuli037@gmail.com>
2022-01-04 06:52:29 +0800

    initial commit day5

---
+++ today/day5.py
@@ -0,0 +1,41 @@
+# remember comparasion & logic operation
+
+# comparasion operation (<,>,=,<=,>=,==,!=,is, is not)
+# logic operation (not, or, and, xor(^))
+
+# remember logic operation
+
+print("===NOT===")
+# not 
+a = True
+
+result = not a # operation logic (not)
+
+print("data:", result)
+
+print("===OR===")
+# or
+a = False
+b = False
+
+result = a or b
+print("data:", result)
+
+print("===AND===")
+# and 
+a = True
+b = True
+
+result = a and b
+
+print("data:", result)
+
+print("===XOR===")
+# xor 
+a = False
+b = True
+
+result = a ^ b
+
+print("data:", result)
+
