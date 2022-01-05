e8f207519f0a95562c9b7e9a6328339e65a6da5f
thujuli <thujuli037@gmail.com>
2022-01-05 11:26:07 +0800

    day6 remember casting type data

--- today/day6.py
+++ today/day6.py
@@ -1,35 +1,62 @@
-# remember type data
+# remember casting type data
 
-# type data in python (integer, float, string, boolean)
-
-# INTEGER 
+# INTEGER
 print("===INTEGER===")
-data_int = 10
+data_int = 10  # TYPE DATA INT
 
-print("data:", data_int,"type data:",type(data_int))
+# CASTING TO TYPE DATA
+data_float = float(data_int)
+data_str = str(data_int)
+data_bool = bool(data_int)
 
-# FLOAT 
+# PRINT CASTING
+print("data:", data_int, "type:", type(data_int))
+print("data:", data_float, "type:", type(data_float))
+print("data:", data_str, "type:", type(data_str))
+print("data:", data_bool, "type:", type(data_bool))
+
+# FLOAT
 print("===FLOAT===")
 data_float = 10.0
 
-print("data:", data_float,"type data:",type(data_float))
+# CASTING TO TYPE DATA
+data_int = int(data_float)
+data_str = str(data_float)
+data_bool = bool(data_float)
 
-# STRING 
+# PRINT CASTING
+print("data:", data_int, "type:", type(data_int))
+print("data:", data_float, "type:", type(data_float))
+print("data:", data_str, "type:", type(data_str))
+print("data:", data_bool, "type:", type(data_bool))
+
+# STRING
 print("===STRING===")
 data_str = "10"
 
-print("data:", data_str,"type data:",type(data_str))
+# CASTING TO TYPE DATA
+data_int = int(data_str)
+data_float = float(data_str)
+data_bool = bool(data_str)
+
+# PRINT CASTING
+print("data:", data_int, "type:", type(data_int))
+print("data:", data_float, "type:", type(data_float))
+print("data:", data_str, "type:", type(data_str))
+print("data:", data_bool, "type:", type(data_bool))
 
 # BOOLEAN
 print("===BOOLEAN===")
 data_bool = True
 
-print("data:", data_bool,"type data:",type(data_bool))
+# CASTING TO TYPE DATA
+data_int = int(data_bool)
+data_float = float(data_bool)
+data_str = str(data_bool)
 
-# SPECIAL TYPE DATA
-from ctypes import c_double
-
-data_c_types = c_double(10.9)
-print("data:", data_c_types,"type data:",type(data_c_types))
-
+# PRINT CASTING
+print("data:", data_int, "type:", type(data_int))
+print("data:", data_float, "type:", type(data_float))
+print("data:", data_str, "type:", type(data_str))
+print("data:", data_bool, "type:", type(data_bool))
 
