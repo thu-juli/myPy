8f55f789c39b9ae7f68a0b2ce1f32b0ee9e984f9
thujuli <thujuli037@gmail.com>
2022-01-20 10:33:36 -0800

    remember operation str

---
+++ today/day11.py
@@ -0,0 +1,19 @@
+# arithmetic ((),**,*,/,//,%,-,+)
+# comparasion (<,>,<=,>=,==,!=,is, is not)
+# logic (not, or, and, xor(^))
+# bitwise (~, |, &, ^) and shifting (<<, >>)
+
+#  operarion bitwise
+a = 5
+b = 1
+c = a | b
+d = a & b
+e = ~ a
+f = 1  # variable for xor 
+g = a ^ f
+print(f'{format(a,"08b")} = {a}')
+print(f'{format(b,"08b")} = {b}')
+print(f'{format(c,"08b")} = {c}')
+print(f'{format(d,"08b")} = {d}')
+print(f'{format(e,"08b")} = {e}')
+print(f'{format(g,"08b")} = {g}')
