84be74f243a1b550778967499ccbc9355fe9bf69
thujuli <thujuli037@gmail.com>
2022-01-20 17:19:39 -0800

    remember all v3

--- today/day11.py
+++ today/day11.py
@@ -4,10 +4,6 @@
 # bitwise (~, |, &, ^) and shifting (<<, >>)
 
 #  operarion bitwise
-from itertools import count
-from traceback import print_tb
-
-
 a = 5
 b = 1
 c = a | b
@@ -49,3 +45,47 @@ full_name = "Putu Julianta"
 count_fullName = full_name.count("u")
 print(f'character "u" in {full_name} are {count_fullName}')
 # remember method in python
+## method startswith & endswith
+full_name = "Putu Julianta"
+cek_start = full_name.startswith("Putu")
+cek_end = full_name.endswith("julianta")
+print(cek_start)
+print(cek_end)
+## method split() & join()
+my_heart = ['i','love','you']
+print(f'{my_heart} is {type(my_heart)}')
+join_myHeart = " ehm ".join(my_heart)
+split_myHeart =join_myHeart.split(" ehm ")
+print(join_myHeart)
+print(split_myHeart)
+## method center(), ljust(), rjust() and strip()
+hai = 'hai'
+hai_center = hai.center(10)
+hai_ljust = hai.ljust(10,"=")
+hai_rjust = hai.rjust(10,"=")
+strip_haiRjust = hai_rjust.strip("=")
+print(hai_center)
+print(hai_ljust)
+print(hai_rjust)
+print(strip_haiRjust)
+# formating string spesific to number
+print(f'\nformating string\n')
+name = "Julianta"
+print("hallo" + " " + name) # not use format
+print(f'hallo {name}') # use format
+# spesific to number
+buy = 50000
+print(f'{buy:,}')
+# decimal 
+decimal = 10.915
+print(f'{decimal} or {decimal:.2f}')
+# landing zero
+print(f'{decimal:010}')
+# show minus (-) and plus (+)
+a = -5
+b = 5
+print(f'{a:-}')
+print(f'{b:+}')
+# show percent (%)
+percent = 0.25
+print(f'{percent:.2%}')
