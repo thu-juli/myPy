72ab7ba6bdc8fca39338b3da2d51b2aa76967de7
thujuli <thujuli037@gmail.com>
2022-01-23 23:14:24 +0800

    introduction basic while loop

--- today/day13.py
+++ today/day13.py
@@ -1,18 +1,17 @@
-# introduce for loop basic
-''''
-for condition:
-    action
+# introduction while loop
 '''
-# looping use list (use when data unordered )
-number_list = [0,2,3,4,5]
-for angka in number_list:
-    print(f'value angka = {angka}')
-print('\n')
-# loping use range() function (use if using data ordered)
-for i in range(1,9):
-    print(f'this is data {i}')
-print('\n')
-# loping using string
-data_str = 'my name is juli'
-for i in data_str:
-    print(i)
+command
+while confition:
+    action 1
+    action 2
+'''
+
+number = 0
+while number < 1:
+    print('Look and see') # :-)
+print(f'value number = {number}')
+while number < 2:
+    number += 1
+    print(f'value number = {number}')
+    print('Hai master')
+print('stop')
