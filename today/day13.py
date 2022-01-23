2495f5bf1c471a763ce1741d52432d9bb7d4fd9f
thujuli <thujuli037@gmail.com>
2022-01-24 00:05:57 +0800

    continue, pass and break

--- today/day13.py
+++ today/day13.py
@@ -1,17 +1,41 @@
-# introduction while loop
-'''
-command
-while confition:
-    action 1
-    action 2
-'''
-
+# control flow (continue, pass, break)
+## pass
 number = 0
-while number < 1:
-    print('Look and see') # :-)
-print(f'value number = {number}')
-while number < 2:
+print(f'The number now is: {number}')
+while number < 5:
     number += 1
-    print(f'value number = {number}')
-    print('Hai master')
-print('stop')
+    print(f'The number now is: {number}')
+    if number == 3:
+        pass  # not execution (use as dummy)
+print('finish\n')
+## continue
+print('continue'.upper().center(10))
+number = 0
+while number < 5:
+    number += 1
+    print(f'The number now is: {number}') # confition1
+    if number == 3:
+        print('Stop, watshupp')
+        continue # jumping to condition1 looping
+    print('whatsupp') # contition2
+print('end program\n')
+## break
+number = 0
+while number < 5:
+    number += 1
+    print(f'The number now is {number}')
+    if number == 3:
+        print('Nice')
+        break
+    print('Whatsupp')
+print('End.')
+## simple program use break
+count = int(input('Count to : '))
+number = 0
+while True:
+    number +=1
+    print(f'{number}')
+    if number == count :
+        print(f'this is {count}')
+        break
+print('endprogram')    
