dfc4a9b56a6fc1d3ecbb223c104140f80302c9ba
thujuli <thujuli037@gmail.com>
2022-01-23 22:50:40 +0800

    introduction basic for loop

--- today/day13.py
+++ today/day13.py
@@ -1,30 +1,18 @@
-# exerise make a simple calculator
+# introduce for loop basic
+''''
+for condition:
+    action
 '''
-1. component req (number1, operator, number2, result)
-2. user input (number1, operator, number2)
-3. process = number1 operatoer number2
-3.1 operator use elif
-'''
-
-print(17*'=')
-print('simple calculator'.upper())
-print(17*'=')
-# input
-number1 = float(input('Please input value for number1\t\t\t: '))
-operator = input('Please input operator arithmetic (+,-,x,/) \t: ')
-number2 = float(input('Please input value for number2\t\t\t: '))
-# process and result
-if operator == '+':
-    result = number1 + number2
-    print(f'Your result : {result:.2f}')
-elif operator == '-':
-    result = number1 - number2
-    print(f'Your result : {result:.2f}')
-elif operator == 'x' or operator == '*':
-    result = number1 * number2
-    print(f'Your result : {result:.2f}')
-elif operator == '/':
-    result = number1 / number2
-    print(f'Your result : {result:.2f}')
-else:
-    print('Please enter the correct operator!!!')
+# looping use list (use when data unordered )
+number_list = [0,2,3,4,5]
+for angka in number_list:
+    print(f'value angka = {angka}')
+print('\n')
+# loping use range() function (use if using data ordered)
+for i in range(1,9):
+    print(f'this is data {i}')
+print('\n')
+# loping using string
+data_str = 'my name is juli'
+for i in data_str:
+    print(i)
