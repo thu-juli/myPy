ed8d59450a588fcbfd00a3a2d56399d4bc5207da
thujuli <thujuli037@gmail.com>
2022-01-23 19:51:04 +0800

    if and else statement

--- today/day13.py
+++ today/day13.py
@@ -1,40 +1,17 @@
-# learn from w3school.com
-## Python list
-fruits = ['apple','banana','cherry']
-print(fruits)
-print(len(fruits))
-print(fruits[0])
-print(fruits[-1])
-print(fruits[0:-1])
-# replace
-print(10*"=")
-fruits = ['apple','banana','cherry']
-fruits[0] = 'water melon'
-print(fruits)
-fruits = ['apple','banana','cherry']
-fruits[0:2] = ['orange', 'melon']
-print(fruits)
-fruits = ['apple','banana','cherry']
-fruits[0:1] = ['avocado', 'melon', 10]
-print(fruits)
-fruits = ['apple','banana','cherry']
-fruits[0:2] = ['strawbery']
-print(fruits)
-# use method
-fruits = ['apple','banana','cherry']
-fruits.insert(1,'watermelon')
-print(fruits)
-## python addlist item (method list)
-### .apped()
-print(f'method list'.capitalize().center(20,'='))
-fruits = ['apple','banana','cherry']
-fruits.append('manggo')
-print(fruits)
-fruits = ['apple','banana','cherry']
-fruits.insert(0,'avocado')
-print(fruits)
-fruits = ['apple','banana','cherry']
-tropical =('mango', 'papaya')
-fruits.extend(tropical)
-print(fruits)
-## python remove specified item
+# if and else statement
+'''
+command to create a if statement
+if [condition] : statemet
+'''
+# if statement inline
+name = input('Please input your name: ')
+if name =='julianta': print('Wellcome back "Master"')
+# if statement indentation
+if name == 'ucup':
+    print("Wellcome ucup")
+    print('your handsome')
+# else statement
+else :
+    print('You are not "julianta" and "ucup"')
+    print(f'thanks {name}')
+print('End program.')
