e2af42f504f188f3cd500acc5752c79adb9790fa
thujuli <thujuli037@gmail.com>
2022-02-01 13:38:09 +0800

    dicoding unittest

---
+++ today/day18/day18_3.py
@@ -0,0 +1,90 @@
+
+# # Unit testing 
+
+# import unittest
+ 
+# class TestStringMethods(unittest.TestCase):
+    
+#     def test_strip(self):
+#         self.assertEqual('www.dicoding.com'.strip('c.mow'), 'dicoding')
+    
+#     def test_isalnum(self):
+#         self.assertTrue('c0d1ng'.isalnum())
+#         self.assertFalse('c0d!ng'.isalnum())
+    
+#     def test_index(self):
+#         s = 'dicoding'
+#         self.assertEqual(s.index('coding'), 2)
+#         # cek s.index gagal ketika tidak ditemukan
+#         with self.assertRaises(ValueError):
+#             s.index('decode')
+    
+# if __name__ == '__main__':
+#     unittest.main()
+
+# # case use unittest
+# import unittest
+ 
+# def koneksi_ke_db():
+#     print('[terhubung ke db]')
+# def putus_koneksi_db(db):
+#     print('[tidak terhubung ke db {}]'.format(db))
+ 
+# class User:
+#     username = ''
+#     aktif = False
+#     def __init__(self, db, username):  # using db sample
+#         self.username = username
+#     def set_aktif(self):
+#         self.aktif = True
+ 
+# class TestUser(unittest.TestCase):
+#     def test_user_default_not_active(self):
+#         db = koneksi_ke_db()
+#         dicoding = User(db, 'dicoding')
+#         self.assertFalse(dicoding.aktif)  # tidak aktif secara default
+#         putus_koneksi_db(db)
+ 
+#     def test_user_is_active(self):
+#         db = koneksi_ke_db()
+#         dicoding = User(db, 'dicoding')
+#         dicoding.set_aktif()  # aktifkan user baru
+#         self.assertTrue(dicoding.aktif)
+#         putus_koneksi_db(db)
+ 
+# if __name__ == '__main__':
+#     unittest.main()
+
+# use method setUp() and tearDown()
+import unittest
+ 
+def koneksi_ke_db():
+    print('[terhubung ke db]')
+def putus_koneksi_db(db):
+    print('[tidak terhubung ke db {}]'.format(db))
+ 
+class User:
+    username = ''
+    aktif = False
+    def __init__(self, db, username):  # using db sample
+        self.username = username
+    def set_aktif(self):
+        self.aktif = True
+
+class TestUser(unittest.TestCase):
+    def setUp(self):
+        self.db = koneksi_ke_db()
+        self.dicoding = User(self.db, 'dicoding')
+
+    def tearDown(self):
+        putus_koneksi_db(self.db)
+
+    def test_user_default_not_active(self):
+        self.assertFalse(self.dicoding.aktif)
+
+    def test_user_is_active(self):
+        self.dicoding.set_aktif()
+        self.assertTrue(self.dicoding.aktif)
+
+if __name__ == '__main__':
+    unittest.main()
