684cb1de7a0133f71b326d17aae8b08192ae6d0d
thujuli <thujuli037@gmail.com>
2022-02-08 02:10:35 +0800

    tambah menu hapus dan cari

--- today/day19/program/app.py
+++ today/day19/program/app.py
@@ -14,8 +14,8 @@ while True:
     print("Menu".center(20,'='))
     print("1. Daftar Kontak")
     print("2. Tambah Kontak")
-    print("3. Cari Kontak")
-    print("4. Hapus Konta")
+    print("3. Hapus Kontak")
+    print("4. Cari Kontak")
     print("0. Keluar")
 
     pilih_menu = input('pilih menu: '.title())
@@ -26,5 +26,13 @@ while True:
     elif pilih_menu == '2':
         kontak = function.new_kontak()
         daftar_kontak.append(kontak)
+    elif pilih_menu == '3':
+        function.hapus_kontak(daftar_kontak)
+    elif pilih_menu == '4':
+        function.cari_kontak(daftar_kontak)
+    else:
+        print("Menu yang anda pilih tidak tersedia")
+
+
 
 print('Keluar dari program, sampai jumpa :)')
--- today/day19/program/function.py
+++ today/day19/program/function.py
@@ -15,3 +15,31 @@ def new_kontak():
         'telepon': telepon 
     }
     return kontak
+
+def hapus_kontak(daftar_kontak):
+    nama = input('Masukan nama kontak yang akan dihapus: ')
+
+    index = -1
+
+    for i in range(0, len(daftar_kontak)):
+        kontak = daftar_kontak[i]
+        if nama == kontak['nama']:
+            index = i
+            break
+    
+    if index == -1:
+        print('Data yang anda cari tidak ditemukan')
+    else:
+        del daftar_kontak[index]
+        print('Data berhasil dihapus')
+
+def cari_kontak(daftar_kontak):
+    search_nama = input('Masukan nama yang dicari: ')
+
+    for kontak in daftar_kontak:
+        nama = kontak['nama']
+        if nama.lower().find(search_nama.lower()) != -1:
+            print('='*20)
+            print(f'Nama: {kontak["nama"]}')
+            print(f'Email: {kontak["email"]}')
+            print(f'Telepon: {kontak["telepon"]}')
