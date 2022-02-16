a3567d7533938cadafb5fbb4a7477e9c193cd39a
thujuli <thujuli037@gmail.com>
2022-02-16 08:05:19 +0800

    import BeautifulSoup and requests

--- today/day21/theory/Project/function.py
+++ today/day21/theory/Project/function.py
@@ -1,3 +1,6 @@
+import requests
+from bs4 import BeautifulSoup
+
 def ekstrasi_data():
     """
     Tanggal: 11 Februari 2022, 21:25:16 WIB
@@ -7,22 +10,37 @@ def ekstrasi_data():
     Pusat: 267 km BaratLaut TAHUNA-KEP.SANGIHE-SULUT
     Keterangan: tidak berpotensi TSUNAMI
     """
+    try:
+        content = requests.get('https://www.bmkg.go.id/')
+    except Exception:
+        return None
+    if content.status_code == 200:
+        print(content.text)
+        # soup = BeautifulSoup(content)
+        # print(soup.prettify())
 
-    data = dict()
-    data['tanggal'] = '11 Februari 2022, 21:25:16 WIB'
-    data['magnitude'] = 5.2
-    data['kedalaman'] = '10 km'
-    data['lokasi'] = {
-    'lu': 5.77,
-    'bt': 124.43 
-    }
-    data['pusat'] = '267 km BaratLaut TAHUNA-KEP.SANGIHE-SULUT'
-    data['keterangan'] = 'tidak berpotensi TSUNAMI'
+        data = dict()
+        data['tanggal'] = '11 Februari 2022, 21:25:16 WIB'
+        data['magnitude'] = 5.2
+        data['kedalaman'] = '10 km'
+        data['lokasi'] = {
+        'lu': 5.77,
+        'bt': 124.43 
+        }
+        data['pusat'] = '267 km BaratLaut TAHUNA-KEP.SANGIHE-SULUT'
+        data['keterangan'] = 'tidak berpotensi TSUNAMI'
 
-    return(data)
+        return(data)
+    else:
+        return None
 
 
 def menampilkan_data(result):
+    if result is None:
+        print('Tidak bisa menampilkan data Gempa Terkini')
+        return
+
+
     print('Aplikasi Deteksi Gempa Terkini')
     print(f"Tanggal: {result['tanggal']}")
     print(f"Magnitude: {result['magnitude']}")
